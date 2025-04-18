2380394a07c548caea69420cc41f6bed2cb73aee
Orel <ryiseld@gmail.com>
2025-04-18 16:46:19 +0300

    feat: show logo in home page

--- apps/web/src/HomePage.tsx
+++ apps/web/src/HomePage.tsx
@@ -1,13 +1,20 @@
 import { Credits } from "@/components/Credits";
 import { ParcelsTable } from "@/components/ParcelsTable";
-import { Container, Flex, Heading } from "@radix-ui/themes";
+import { Box, Container, Flex, Heading } from "@radix-ui/themes";
 
 export function HomePage() {
   return (
     <Container size="3">
-      <Flex direction="column" gap="4" mt="6">
-        <Heading size="5">Parcels</Heading>
-        <ParcelsTable />
+      <Flex direction="column" gap="4">
+        <Flex align="center" gap="4" my="4">
+          <img src="/logo.png" alt="Parcels" width={42} height={42} />
+          <Heading size="5">Parcels</Heading>
+        </Flex>
+
+        <Box>
+          <ParcelsTable />
+        </Box>
+
         <Flex justify="end">
           <Credits />
         </Flex>
