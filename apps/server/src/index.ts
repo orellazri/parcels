508e6629187fd46e7d50b0204a4bec8cc483d165
Orel <ryiseld@gmail.com>
2025-04-18 18:12:09 +0300

    fix: change server host to 0.0.0.0

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -55,7 +55,7 @@ async function main() {
     true, // waitForCompletion
   );
 
-  server.listen({ port: 3000 }, (err, address) => {
+  server.listen({ host: "0.0.0.0", port: 3000 }, (err, address) => {
     if (err) {
       console.error(err);
       process.exit(1);
