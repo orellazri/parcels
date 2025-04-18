87677a3ae68a70f0f4b3b8238ca01d327a8545f2
Orel <ryiseld@gmail.com>
2025-04-18 18:02:15 +0300

    feat: migrate database on server start

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,5 +1,6 @@
 import fastifyStatic from "@fastify/static";
 import { CronJob } from "cron";
+import { migrate } from "drizzle-orm/node-postgres/migrator";
 import fastify from "fastify";
 import path from "path";
 import { default as dbPlugin, drizzleDb } from "./plugins/db";
@@ -7,48 +8,60 @@ import { creditsRoutes } from "./routes/credits";
 import { parcelsRoutes } from "./routes/parcels";
 import { refreshParcels } from "./services/parcels";
 
-const server = fastify();
-
-server.register(dbPlugin);
-
-// React SPA
-server.register(fastifyStatic, { root: path.join(__dirname, "..", "web") });
-
-// API
-server.register(parcelsRoutes, { prefix: "/api/parcels" });
-server.register(creditsRoutes, { prefix: "/api/credits" });
-
-// Healthcheck
-server.get("/health", async (request, reply) => {
-  return reply.status(200).send({ status: "ok" });
-});
-
-// Refresh parcels in the background
-new CronJob(
-  process.env.REFRESH_PARCELS_CRON ?? "0 0 */3 * * *",
-  async function () {
-    console.log("Refreshing parcels...");
-    try {
-      const numCreated = await refreshParcels(drizzleDb);
-      console.log(`Refreshed parcels. Created ${numCreated} parcels`);
-    } catch (error) {
-      console.error("Error refreshing parcels", error);
-    }
-  }, // onTick
-  null, // onComplete
-  true, // start
-  process.env.TIMEZONE ?? "UTC", // timeZone
-  null, // context
-  process.env.ENV !== "development", // runOnInit
-  null, // utcOffset
-  null, // unrefTimeout
-  true, // waitForCompletion
-);
-
-server.listen({ port: 3000 }, (err, address) => {
-  if (err) {
-    console.error(err);
+async function main() {
+  try {
+    console.info("Migrating database");
+    await migrate(drizzleDb, { migrationsFolder: path.join(__dirname, "..", "drizzle") });
+  } catch (error) {
+    console.error("Error migrating database", error);
     process.exit(1);
   }
-  console.log(`Server listening at ${address}`);
-});
+
+  const server = fastify();
+
+  server.register(dbPlugin);
+
+  // React SPA
+  server.register(fastifyStatic, { root: path.join(__dirname, "..", "web") });
+
+  // API
+  server.register(parcelsRoutes, { prefix: "/api/parcels" });
+  server.register(creditsRoutes, { prefix: "/api/credits" });
+
+  // Healthcheck
+  server.get("/health", async (request, reply) => {
+    return reply.status(200).send({ status: "ok" });
+  });
+
+  // Refresh parcels in the background
+  new CronJob(
+    process.env.REFRESH_PARCELS_CRON ?? "0 0 */3 * * *",
+    async function () {
+      console.log("Refreshing parcels...");
+      try {
+        const numCreated = await refreshParcels(drizzleDb);
+        console.log(`Refreshed parcels. Created ${numCreated} parcels`);
+      } catch (error) {
+        console.error("Error refreshing parcels", error);
+      }
+    }, // onTick
+    null, // onComplete
+    true, // start
+    process.env.TIMEZONE ?? "UTC", // timeZone
+    null, // context
+    process.env.ENV !== "development", // runOnInit
+    null, // utcOffset
+    null, // unrefTimeout
+    true, // waitForCompletion
+  );
+
+  server.listen({ port: 3000 }, (err, address) => {
+    if (err) {
+      console.error(err);
+      process.exit(1);
+    }
+    console.log(`Server listening at ${address}`);
+  });
+}
+
+main();
